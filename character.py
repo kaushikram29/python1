dbd69ed1152f5c43110579d00ea7547b4b4b6ebb
kaushikram29 <37103207+kaushikram29@users.noreply.github.com>
2019-06-06 10:16:28 +0530

    Create character.py

---
+++ character.py
@@ -0,0 +1,7 @@
+X=input("Enter your character")
+if((X>="a" and X<="z) or (X>="A" and X<="Z")):
+  print(X, "it is an alphabet")
+elif ((X>=0 and Z<=9)):
+  print(X ,"it is a number or digit")
+else:
+  print(X,"it is not alphabet or digit")
