c63a36309d6acb1505c1a3c24aa0dd69fe0a944d
kaushikram29 <37103207+kaushikram29@users.noreply.github.com>
2019-06-07 12:02:51 +0530

    Create VALUES.py

---
+++ VALUES.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=input().split()
+k=[]
+for i in range(0,a):
+  if(int(b[i])==i):
+    k.append(b[i])
+if(k==[]):
+  print("-1")
+else:
+  k.sort()
+  for j in range(0,len(k)):
+    print(k[j],end=" ")
